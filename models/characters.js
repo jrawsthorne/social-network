cbc98888afd43b3f51a1e633bf893b90c2e3a879
Alex Yates <adyates1@sheffield.ac.uk>
2020-03-15 16:42:00 +0000

    Moved to Fabio's format added mongo

---
+++ models/characters.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var Character = new Schema(
+    {
+        first_name: {type: String, required: true, max: 100},
+        family_name: {type: String, required: true, max: 100},
+        dob: {type: Number},
+        whatever: {type: String} //any other field
+    }
+);
+
+// Virtual for a character's age
+Character.virtual('age')
+    .get(function () {
+        const currentDate = new Date().getFullYear();
+        const result= currentDate - this.dob;
+        return result;
+    });
+
+Character.set('toObject', {getters: true, virtuals: true});
+
+
+var characterModel = mongoose.model('Character', Character );
+
+module.exports = characterModel;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser= require("body-parser");
+
+
+var character = require('../controllers/characters');
+var initDB= require('../controllers/init');
+initDB.init();
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'My Form' });
 });
+
+router.post('/index', character.getAge);
+
+
+/* GET home page. */
+router.get('/insert', function(req, res, next) {
+  res.render('insert', { title: 'My Form' });
+});
+
+router.post('/insert', character.insert);
+
 
 module.exports = router;
